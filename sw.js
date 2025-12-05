0dc7addf301855d2736024b4b687ab6d5bc2f81d
NHZallen <130979060+NHZallen@users.noreply.github.com>
2025-12-05 12:05:51 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js
-const CACHE_NAME = 'astra-music-v6';
+const CACHE_NAME = 'astra-music-v7';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
@@ -42,6 +42,7 @@ self.addEventListener('activate', (event) => {
   );
 
 });
+
 
 
 
